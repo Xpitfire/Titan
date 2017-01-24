4ff098fda411a9b181459d3dbc9a6c5b3cb8c850
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2017-01-24 13:21:55 +0100

    Added service projects

---
+++ src/Titan.Service.Impl.HeuristicLab/EvaluationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Titan.Core.Syntax;
+using Titan.Service;
+
+namespace Titan.Service.Impl.HeuristicLab
+{
+    public class EvaluationService : IEvaluationService
+    {
+        public double Evaluate(NetworkSyntax node)
+        {
+            return 0.0;
+        }
+
+        public string GraphDescription(NetworkSyntax node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Titan.Service/IEvaluationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Titan.Core.Syntax;
+
+namespace Titan.Service
+{
+    public interface IEvaluationService
+    {
+        double Evaluate(NetworkSyntax node);
+        string GraphDescription(NetworkSyntax node);
+    }
+}
