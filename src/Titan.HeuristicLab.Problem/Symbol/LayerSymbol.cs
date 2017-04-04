aeedf205bd6965a4963dad06b3bd4138a1c8c825
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2017-04-04 14:27:08 +0200

    Added symbol

---
+++ src/Titan.HeuristicLab.Problem/Symbol/LayerSymbol.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HeuristicLab.Common;
+using HeuristicLab.Encodings.SymbolicExpressionTreeEncoding;
+using HeuristicLab.Persistence.Default.CompositeSerializers.Storable;
+
+namespace Titan.HeuristicLab.Problem
+{
+    [StorableClass]
+    public class ConvSym
+    {
+
+
+    }
+}
